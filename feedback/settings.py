06b71cb208b11b22688b2dba877549b447083480
Waseem Ashraf <waseemashraf.99@gmail.com>
2022-11-13 23:11:37 +0530

    Added profile app to handle images

--- feedback/settings.py
+++ feedback/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'profiles',
     'reviews',
     "django.contrib.admin",
     "django.contrib.auth",
@@ -116,3 +117,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/user-media/"
---
+++ profiles/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
+
+from .forms import ProfileForm
+from .models import UserProfile
+# Create your views here.
+
+# def store_file(file):
+#     with open("temp/image.jpeg","wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk) Dont need this now
+
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
+
+# class CreateProfileView(View):
+#     def get(self,request):
+#         form = ProfileForm()
+#         return render(request,"profiles/create_profile.html", {
+#             "form":form
+#         })
+
+#     def post(self,request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+
+#         if submitted_form.is_valid():
+#             # store_file(request.FILES["image"])
+#             profile = UserProfile(image= request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
+        
+#         else:
+#             return render(request,"profiles/create_profile.html", {
+#             "form":submitted_form
+#             })
+
+class ProfileView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
